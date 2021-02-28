1689a2368e5daf6586dad12a9be545aca3b4eba6
Jenn Nelson <jnelson@edenpr.org>
2021-02-28 12:39:30 -0600

    Create Abecedarian.java

---
+++ Abecedarian.java
@@ -0,0 +1,12 @@
+public class Abecedarian {
+
+	public static void main(String[] args) {
+		
+		System.out.println(isAbecedarian("abdest") + " true");
+		System.out.println(isAbecedarian("acknow") + " true");
+		System.out.println(isAbecedarian("acorsy") + " true");
+		System.out.println(isAbecedarian("nelson") + " false");
+		System.out.println(isAbecedarian("java") + " false");
+	}
+  
+}
